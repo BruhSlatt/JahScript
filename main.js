fc32a7c13a7c46c55ef9adac4a04d45dbbbfe2e5
Tony <tony.n.flpa@gmail.com>
2019-06-18 13:36:14 -0400

    basic js stuff

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
-alert("Hello Jerry!")
+// Objects
+let car = {
+// Properties
+    color: "red",
+    engine: {cylinders: 4, turdo: false}
+
+// Methods
+    
+}
+
+
+
+
+
+let multiDimensional = ['something',['somethingElse']]
+
+
+
+// array o' objects
+let people = [{name: "isaiah", age:17},
+            {name: "alan", age:28},
+            {name:'connor', age:17},
+            {name: "Carlos", age:17},
+            {name: "Bren", age:"eternal"},
+            {name:'Tony', age:17} 
+
+            ]
+
+
+// for loop
+for (let i=0; i < people.length; i++) {
+    console.log(people[i].age)
+}
+
+
+
+
+
+// write a program that find the summation of every number from 1-num
+
+//counting challenge for loops
+function summation(num){
+    if(num  == 1)
+      return num
+    let total = 0;
+    for(let i=0; i < num;i++){
+        total= i + total
+    }
+        return total+num
+    }
+  
+  
+  summation(26)
+
+    
